0f7c3f79728b1d6401e9b2cfd27bcd7d7e762a34
canberko560 <canberkvarli@gmail.com>
2021-08-05 13:08:46 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@
 
 canberk = User.create!([{
             username: 'canberko560',
+            password_digest: "some_digest",
             artist_name: 'Ebu Zehir',
             age: '25',
             bio: 'An artist on a journey to uncover the magical truth by mixing authentic sounds together and create the world of music.',
@@ -20,18 +21,22 @@ canberk = User.create!([{
             first_name: 'Canberk',
             last_name: 'Varli', } ]);
 
+
 taleofus = User.create!([{
             username: 'taleofus99',
+            password_digest: "some_digest",
             artist_name: 'Tale of Us',}]);
 
 muse = User.create!([{
             username: 'MattChrisDom',
+            password_digest: "some_digest",
             artist_name: 'MUSE',
             city: "Glasgow",
             country: "Scotland"}]);
 
 aurora = User.create!([{
             username: 'longlivethetrees', 
+            password_digest: "some_digest",
             artist_name: 'Aurora'
             }]);
 
@@ -40,18 +45,18 @@ aurora = User.create!([{
 
 track1 = Track.create!([{
     title: "W.R.A.A.T.S",
-    artist_id: canberk.id,
+    artist_id: 5,
     description: "(W)hat (R)emains (A)fter (A) (T)hunder (S)trike"
 }])
 
 track2 = Track.create!([{
     title: "Boiler Plate",
-    artist_id: muse.id,
+    artist_id: 4,
     description: "Mixed with effort and detailed work, this song is the everything a person will need before a good start."
 }])
 
 track3 = Track.create!([{
     title: "Running with The Wolves",
-    artist_id: muse.id,
+    artist_id: 3,
     description: "Only Love"
 }])
