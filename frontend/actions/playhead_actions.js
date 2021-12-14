638082d40d9df3758846e65a832a792a7ecb8413
canberko560 <canberkvarli@gmail.com>
2021-12-14 19:06:11 +0300

    playhead actions and reducer

---
+++ frontend/actions/playhead_actions.js
@@ -0,0 +1,30 @@
+
+export const RECEIVE_CURRENT_TRACK = "RECEIVE_CURRENT_TRACK";
+export const PAUSE_TRACK = "PAUSE_TRACK";
+export const PLAY_TRACK = "PLAY_TRACK";
+export const SEEK_TRACK = "SEEK_TRACK";
+
+
+export const receiveCurrentTrack = (track) => ({
+    type: RECEIVE_CURRENT_TRACK,
+    track
+})
+
+export const seekTrack = (progress) => ({
+  type: SEEK_TRACK,
+  progress,
+});
+
+export const pauseTrack = () => ({
+    type: PAUSE_TRACK
+})
+
+export const playTrack = () => ({
+    type: PLAY_TRACK
+})
+
+
+
+export const setCurrentTrack = (track) => dispatch => dispatch(receiveCurrentTrack(track));
+export const setCurrentProgress = (progress) => (dispatch) =>
+  dispatch(seekTrack(progress));
---
+++ frontend/reducers/playhead_reducer.js
@@ -0,0 +1,38 @@
+import {RECEIVE_CURRENT_TRACK, PAUSE_TRACK, PLAY_TRACK, SEEK_TRACK} from "../actions/playhead_actions";
+
+
+const _nullTrack = {
+  currentTrack: null,
+  paused: true,
+  currentTime: 0
+}; 
+
+export default (state = _nullTrack, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+
+  switch (action.type) {
+    case PLAY_TRACK:
+      return Object.assign(newState, {
+        paused: false,
+        currentTrack: state.currentTrack,
+        currentTime: state.currentTime,
+      });
+
+    case PAUSE_TRACK:
+      return Object.assign(newState, {
+        paused: true,
+        currentTrack: state.currentTrack,
+        currentTime: state.currentTime,
+      });
+    case RECEIVE_CURRENT_TRACK:
+      return Object.assign(newState, {
+        currentTrack: action.track,
+        currentTime: 0,
+      });
+    case SEEK_TRACK:
+      return Object.assign(newState, { currentTime: action.progress });
+    default:
+      return state;
+  }
+};
