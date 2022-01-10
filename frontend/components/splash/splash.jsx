d8573faec374c582f760947a60174fd79260b20f
canberko560 <canberkvarli@gmail.com>
2022-01-10 16:24:30 +0300

    add splash text, button and image

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,9 +4,21 @@ import React from 'react';
 const Splash = () => {
 
     return (
-        <div>
-            Saying hello from Splash
-        </div>
+        <>
+            <div className='splash-text-container'>
+                <span className='splash-text' id='splash-text-big'>
+                    Calling all creators
+                </span>
+                <h1 className='splash-text' id="splash-text-small">
+                    Get on SoundCloud to connect with fans, share your sounds, and grow your audience. What are you waiting for?
+                </h1>
+                <div className='splash-button-container'>
+                    <button className='splash-button'>
+                            Find out more
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
 
--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -1,2 +1,3 @@
 import { connect } from "react-redux";
 
+
