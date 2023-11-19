69835b7bab329f8072cf721ca1b9f5d78ba197a8
Canberk Varli <canberkvarli@gmail.com>
2023-11-18 17:23:36 -0800

    remove npm code from build

--- bin/sonar-build.sh
+++ bin/sonar-build.sh
@@ -3,11 +3,6 @@
 set -o errexit
 
 bundle install
-# clean
-rm -rf public
-# build
-npm install --prefix frontend && npm run build --prefix frontend
-# migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
-# postbuild
-cp -a frontend/build/. public/
